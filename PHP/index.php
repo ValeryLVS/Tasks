49f8a1da2ddadbae86556ea47a2fb040c9647e29
mrvalerylv <mrvalerylv@yandex.ru>
2022-10-11 14:42:16 +0300

    Josephus Permutation

--- PHP/index.php
+++ PHP/index.php
@@ -1,6 +1,6 @@
 <?php
 
-//На вход две матрциы (многмерные массивы) на выходе их сумма
+//На вход две матрциы (многомерные массивы) на выходе их сумма
 // |1 2 3|     |2 2 1|     |1+2 2+2 3+1|     |3 4 4|
 // |3 2 1|  +  |3 2 3|  =  |3+3 2+2 1+3|  =  |6 4 4|
 // |1 1 1|     |1 1 3|     |1+1 1+1 1+3|     |2 2 4|
@@ -170,6 +170,25 @@ function fileNameExtractorTwo(string $dirtyFileName): string
 {
     preg_match('/\d+_([^.]*).([^.]*).*/', $dirtyFileName, $m);
     return "{$m[1]}.{$m[2]}";
+}
+
+
+//[1,2,3,4,5,6,7] - initial sequence
+//[1,2,4,5,6,7] => 3 is counted out and goes into the result [3]
+//[1,2,4,5,7] => 6 is counted out and goes into the result [3,6]
+//[1,4,5,7] => 2 is counted out and goes into the result [3,6,2]
+//[1,4,5] => 7 is counted out and goes into the result [3,6,2,7]
+//[1,4] => 5 is counted out and goes into the result [3,6,2,7,5]
+//[4] => 1 is counted out and goes into the result [3,6,2,7,5,1]
+//[] => 4 is counted out and goes into the result [3,6,2,7,5,1,4]
+function josephus(array $items, int $k): array {
+    $result = [];
+    $keyPos = 0;
+    while (count($items)) {
+        $keyPos = ($keyPos + $k - 1) % count($items);
+        $result[] = array_splice($items, $keyPos, 1)[0];
+    }
+    return $result;
 }
 
 ?>
